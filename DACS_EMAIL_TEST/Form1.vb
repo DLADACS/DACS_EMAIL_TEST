112df77f8d9f1d37ec5ee38dc544b1769eeea0f0
luke peterson <eoclansyr@gmail.com>
2020-01-23 15:09:32 -0600

    updated nugets and removed async call for testing

--- DACS_EMAIL_TEST/Form1.vb
+++ DACS_EMAIL_TEST/Form1.vb
@@ -1,14 +1,14 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim Dacs1 As New DACS.Proxy.Email(DACS.ENUMS.DMZ.PROD_Public_DACS, "FLSKFJHALDFKJHASDF")
+        Dim Dacs1 As New DACS.Proxy.Email(DACS.ENUMS.DMZ.STAGE_Public_DACS, "10")
         Dim dacsemail As New DACS.Email.EmailToSend
-        dacsemail.AppKey = "sdagfsdgasdg"
+        'dacsemail.AppKey = "10"
         dacsemail.EmailBody = "JUNK TO SEND in BODY"
         dacsemail.EmailFrom = "j62bb.dacs@dla.mil"
         dacsemail.EmailTo = "Christopher.hood.ctr@dla.mil"
         dacsemail.EmailBCC = "chood@cna-consulting.com"
         dacsemail.EmailCC = "majianlord@gmail.com"
         dacsemail.EmailSubject = "This is a Test Email"
-        Dim task1 As Task(Of DACS.Email.EmailStatus) = Dacs1.SendEmailAsync(dacsemail)
+        Dim task1 As DACS.Email.EmailStatus = Dacs1.SendEmail(dacsemail)
     End Sub
 End Class
